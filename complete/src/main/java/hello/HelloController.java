1b054ba46a5b3cc1786236ef8486bec05bb6a74a
Abdoo Mahran <abdoo.mahran@gmail.com>
2017-08-14 22:48:56 -0400

    getting hostname and IP of the running server

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -1,14 +1,29 @@
 package hello;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
     
+	InetAddress ip;
+	String ipHost;
+	String hostname;
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+    	try{
+    	ip = InetAddress.getLocalHost();
+    	ipHost = ip.getHostAddress();
+    	hostname = ip.getHostName();
+        return "Greetings from Azure ===> " + hostname + "  ====> " + ipHost;
+    	}
+    	catch(UnknownHostException e){
+    		e.printStackTrace();
+    		return "Greetings from Azure";
+    	}
     }
     
 }
